290d96032f77cac9f0b4dab78bdc3c0180010bf5
Milan Burda <milan.burda@gmail.com>
2018-10-08 07:36:48 +0200

    feat: add remote.require() / remote.getGlobal() filtering

--- atom/browser/api/atom_api_web_contents.cc
+++ atom/browser/api/atom_api_web_contents.cc
@@ -1591,6 +1591,11 @@ void WebContents::TabTraverse(bool reverse) {
   web_contents()->FocusThroughTabTraversal(reverse);
 }
 
+v8::Local<v8::Object> WebContents::CreateEvent() {
+  return mate::internal::CreateJSEvent(isolate(), GetWrapper(), nullptr,
+                                       nullptr);
+}
+
 bool WebContents::SendIPCMessage(bool internal,
                                  bool send_to_all,
                                  const std::string& channel,
@@ -2057,6 +2062,7 @@ void WebContents::BuildPrototype(v8::Isolate* isolate,
       .SetMethod("focus", &WebContents::Focus)
       .SetMethod("isFocused", &WebContents::IsFocused)
       .SetMethod("tabTraverse", &WebContents::TabTraverse)
+      .SetMethod("_createEvent", &WebContents::CreateEvent)
       .SetMethod("_send", &WebContents::SendIPCMessage)
       .SetMethod("sendInputEvent", &WebContents::SendInputEvent)
       .SetMethod("beginFrameSubscription", &WebContents::BeginFrameSubscription)
